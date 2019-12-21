00e5781e1f9bbf24f45cc15e19af9f9200cf1611
mleemc <mlee@marketchorus.com>
2019-12-21 13:02:52 -0600

    Styling and tile layout

--- pages/dq-the-heart-of-fire/_id.vue
+++ pages/dq-the-heart-of-fire/_id.vue
@@ -1,7 +1,149 @@
 <template>
   <div class="container">
     <Navbar />
-    <div>Hero id: {{ heroId }}</div>
+    <div>Hero id: {{ heroId }} ({{ hero.id }})</div>
+    <div>Gamebook: {{ hero.gamebook }}</div>
+    <div class="tile is-ancestor hero-container">
+      <div class="tile is-vertical is-6">
+        <!-- left panel -->
+        <div class="tile is-parent is-wrapping">
+          <!-- name -->
+          <div class="tile is-child is-12">
+            <div class="rustic-box">
+              <span class="dq-label">Name:</span>
+              <span class="dq-hero-name">{{ hero.name }}</span>
+            </div>
+          </div>
+          <!-- main equipment 1 -->
+          <div class="tile is-child is-4">
+            <div class="rustic-box equipment-box">
+              <span class="dq-label">CLOAK</span>
+            </div>
+          </div>
+          <div class="tile is-child is-4">
+            <div class="rustic-box equipment-box">
+              <span class="dq-label">HEAD</span>
+            </div>
+          </div>
+          <div class="tile is-child is-4">
+            <div class="rustic-box equipment-box">
+              <span class="dq-label">GLOVES</span>
+            </div>
+          </div>
+          <div class="tile is-child is-4">
+            <div class="rustic-box equipment-box">
+              <span class="dq-label">MAIN HAND</span>
+            </div>
+          </div>
+          <div class="tile is-child is-4">
+            <div class="rustic-box equipment-box">
+              <span class="dq-label">CHEST</span>
+            </div>
+          </div>
+          <div class="tile is-child is-4">
+            <div class="rustic-box equipment-box">
+              <span class="dq-label">LEFT HAND</span>
+            </div>
+          </div>
+          <div class="tile is-child is-4">
+            <div class="rustic-box equipment-box">
+              <span class="dq-label">TALISMAN</span>
+            </div>
+            <div class="tile">
+              <div class="tile is-child">
+                <div class="rustic-box attribute-box">
+                  <span class="dq-label">SPEED</span>
+                  <div class="dq-attribute">{{ hero.speed }}</div>
+                </div>
+              </div>
+              <div class="tile is-child">
+                <div class="rustic-box attribute-box">
+                  <span class="dq-label">BRAWN</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tile is-child is-4">
+            <div class="rustic-box equipment-box">
+              <span class="dq-label">FEET</span>
+            </div>
+            <div class="tile">
+              <div class="tile is-child">
+                <div class="rustic-box attribute-box">
+                  <span class="dq-label">MAGIC</span>
+                </div>
+              </div>
+              <div class="tile is-child">
+                <div class="rustic-box attribute-box">
+                  <span class="dq-label">ARMOR</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tile is-child is-4">
+            <div class="rustic-box equipment-box">
+              <span class="dq-label">MONEY POUCH</span>
+            </div>
+            <div class="tile">
+              <div class="tile is-child">
+                <div class="rustic-box attribute-box">
+                  <span class="dq-label">HEALTH</span>
+                </div>
+              </div>
+              <div class="tile is-child">
+                <div class="rustic-box attribute-box">
+                  <span class="dq-label">MAX</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="tile is-vertical is-6">
+        <!-- right panel -->
+        <div class="tile">
+          <div class="tile is-parent">
+            <div class="tile is-child is-12">
+              <div class="columns rustic-box">
+                <div class="column no-padding">
+                  <span class="dq-label">Path:</span>
+                  <span class="dq-hero-name">{{ hero.path }}</span>
+                </div>
+                <div class="column no-padding">
+                  <span class="dq-label">Career:</span>
+                  <span class="dq-hero-name">{{ hero.career }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div>Speed: {{ hero.speed }}</div>
+    <div>Brawn: {{ hero.brawn }}</div>
+    <div>Magic: {{ hero.magic }}</div>
+    <div>Armor: {{ hero.armor }}</div>
+    <div>Health: {{ hero.health }}</div>
+    <div>Max health: {{ hero.healthMax }}</div>
+    <div>
+      eqipmentSlots:
+      <span v-for="slot in hero.eqipmentSlots" :key="slot"> {{ slot }}, </span>
+    </div>
+    <div>
+      eqipmentItems:
+      <span v-for="item in hero.eqipmentItems" :key="item.name">
+        {{ item }},
+      </span>
+    </div>
+    <div>
+      backpackItems:
+      <span v-for="item in hero.backpackItems" :key="item.name">
+        {{ item }},
+      </span>
+    </div>
+    <div>Money pouch: {{ hero.moneyPouch }}</div>
+    <div>Notes: {{ hero.notes }}</div>
   </div>
 </template>
 
@@ -27,6 +169,54 @@ export default {
     },
   },
 }
+/*
+populate attributes
+layout right side
+populate eqipment and items, assign to slots
+multi-panel view
+hand font for entries
+fantasy serif font for labels
+*/
 </script>
 
-<style></style>
+<style>
+.is-wrapping {
+  flex-wrap: wrap;
+}
+.no-padding {
+  padding: 0;
+}
+.rustic-box {
+  margin: 0.3rem;
+  padding: 0.3rem;
+  border: solid 2px #666;
+  border-radius: 255px 9px 225px 9px/9px 225px 9px 255px;
+  overflow: auto;
+}
+.equipment-box {
+  height: 9rem;
+}
+.attribute-box {
+  height: 4rem;
+}
+.hero-container {
+  font: 1em serif;
+}
+.dq-label {
+  text-transform: uppercase;
+  font-variant-position: super;
+  font-size: 0.7em;
+  display: inline-block;
+  vertical-align: top;
+}
+.dq-hero-name {
+  font-size: 1.6em;
+  line-height: 0.8em;
+}
+.dq-attribute {
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  font-size: 1.6em;
+}
+</style>
--- store/dq-the-heart-of-fire.js
+++ store/dq-the-heart-of-fire.js
@@ -3,11 +3,59 @@ export const state = () => ({
     id: '1',
     gamebook: 'dq-the-heart-of-fire',
     name: 'Stolen',
+    path: 'Rogue',
+    career: 'Shadowstalker',
+    speed: 0,
+    brawn: 0,
+    magic: 0,
+    armor: 0,
+    health: 30,
+    healthMax: 30,
+    eqipmentSlots: [
+      'cloak',
+      'head',
+      'gloves',
+      'main hand',
+      'chest',
+      'left hand',
+      'talisman',
+      'feet',
+      'necklace',
+      'ring 1',
+      'ring 2',
+    ],
+    equipmentItems: [],
+    backpackItems: [], // strings
+    moneyPouch: '',
+    notes: '',
+  },
+  eqipmentItem: {
+    slot: '',
+    name: '',
+    attributeBonuses: [],
+    abilities: [], // strings
+  },
+  ability: {
+    type: '',
+    name: '',
+  },
+  attributeBonus: {
+    name: '',
+    modifier: 0,
   },
 })
 
 export const mutations = {}
 
 export const getters = {
-  hero: state => state,
+  hero: state => state.hero,
+  abilitiesSpeed: state =>
+    state.equipmentItems.reduce((items, item) => {
+      return items.concat(
+        item.abilities.filter(ability => ability.type === 'speed'),
+      )
+    }),
+  abilitiesCombat: state => state,
+  abilitiesPassive: state => state,
+  abilitiesModifier: state => state,
 }
